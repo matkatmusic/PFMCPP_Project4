a93e20bf029352c7761577a51e9188ce37991944
Charles Schiermeyer <matkatmusic@gmail.com>
2020-02-18 19:42:09 -0800

    Added Part1 Instructions

--- main.cpp
+++ main.cpp
@@ -1,7 +1,44 @@
-#include <iostream>
+/*
+ Project 4 - Part 1 / 9
+ video: Chapter 2 Part 7
+ Returning Values tasks 
 
+ Create a branch named Part1
+ 
+ 1) write 3 UDTs named FloatType, DoubleType, IntType.
+ 
+ 2) give each UDT the following member functions:
+        add( lhs, rhs );
+        subtract( lhs, rhs );
+        multiply( lhs, rhs );
+        divide( lhs, rhs );
+ 3) implement the appropriate action in the member function. 
+         Be aware that floating point division by 0 is legal, but integer division by 0 will crash your program.  
+         Handle this possible input when you write your divide() functions.
+ 
+ 4) make them return the correct primitive type. e.g. if you're implementing the FloatType::add function, your implementation would start like this:
+        float FloatType::add( float lhs, float rhs )
+ 
+ 5) declare a few instances of each class in main() and call the member functions with the appropriate arguments.
+        FloatType ft;
+        auto result = ft.add(3.2f, 23.f );
+ 
+ 6) print out those results using std::cout:
+        std::cout "result of ft.add(): " << result << std::endl;
+ 
+ 7) After you finish defining each type/function, click the [run] button.  Clear up any errors or warnings as best you can.
+ 
+ Commit your changes by clicking on the Source Control panel on the left, entering a message, and click [Commit and push].
+ 
+Make a pull request after you make your first commit and pin the pull request link to our DM thread.
+
+send me a DM to check your pull request
+
+ Wait for my code review.
+ */
+
+#include <iostream>
 int main()
 {
     std::cout << "good to go!" << std::endl;
-    return 0;    
 }
