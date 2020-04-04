4eb174f29905b4a2e147f9062ed1c14162cd2e6a
Joseph Skarulis <info@dpsproservices.com>
2020-04-04 17:53:24 +0000

    Project 4 - Part 1 Returning Values tasks.

--- main.cpp
+++ main.cpp
@@ -42,7 +42,150 @@ send me a DM to check your pull request
  */
 
 #include <iostream>
+
+struct FloatType
+{
+    float add( float lhs, float rhs );
+    float subtract( float lhs, float rhs );
+    float multiply( float lhs, float rhs );
+    float divide( float lhs, float rhs );
+};
+
+struct DoubleType
+{
+    double add( double lhs, double rhs );
+    double subtract( double lhs, double rhs );
+    double multiply( double lhs, double rhs );
+    double divide( double lhs, double rhs );
+};
+
+struct IntType
+{
+    int add( int lhs, int rhs );
+    int subtract( int lhs, int rhs );
+    int multiply( int lhs, int rhs );
+    int divide( int lhs, int rhs );
+};
+
+float FloatType::add( float lhs, float rhs )
+{
+    return lhs + rhs;
+}
+
+float FloatType::subtract( float lhs, float rhs )
+{
+    return lhs - rhs;
+}
+
+float FloatType::multiply( float lhs, float rhs )
+{
+    return lhs * rhs;
+}
+
+float FloatType::divide( float lhs, float rhs )
+{
+    if ( rhs <= 0.0f )
+    {
+        std::cout << "Warning division by zero." << std::endl;
+    }
+    return lhs / rhs;
+}
+
+double DoubleType::add( double lhs, double rhs )
+{
+    return lhs + rhs;
+}
+
+double DoubleType::subtract( double lhs, double rhs )
+{
+    return lhs - rhs;
+}
+
+double DoubleType::multiply( double lhs, double rhs )
+{
+    return lhs * rhs;
+}
+
+double DoubleType::divide( double lhs, double rhs )
+{
+    if ( rhs <= 0.0 )
+    {
+        std::cerr << "Warning division by zero." << std::endl;
+    }
+    return lhs / rhs;
+}
+
+int IntType::add( int lhs, int rhs )
+{
+    return lhs + rhs;
+}
+
+int IntType::subtract( int lhs, int rhs )
+{
+    return lhs - rhs;
+}
+
+int IntType::multiply( int lhs, int rhs )
+{
+    return lhs * rhs;
+}
+
+int IntType::divide( int lhs, int rhs )
+{
+    if ( rhs <= 0.0 )
+    {
+        std::cerr << "Warning division by zero." << std::endl;
+        return lhs;
+    } 
+    else 
+    {
+        return lhs / rhs;
+    }
+}
+
 int main()
 {
+    FloatType ft;
+
+    auto resultFT = ft.add(3.2f, 23.f );       
+    std::cout << "result of ft.add(): " << resultFT << std::endl;
+
+    resultFT = ft.subtract(3.2f, 23.f );
+    std::cout << "result of ft.subtract(): " << resultFT << std::endl;
+
+    resultFT = ft.multiply(3.2f, 23.f );        
+    std::cout << "result of ft.multiply(): " << resultFT << std::endl;
+
+    resultFT = ft.divide(3.2f, 23.f );     
+    std::cout << "result of ft.divide(): " << resultFT << std::endl;
+
+    DoubleType dt;
+
+    auto resultDT = dt.add(3.2, 23. );       
+    std::cout << "result of dt.add(): " << resultDT << std::endl;
+
+    resultDT = dt.subtract(3.2, 23. );
+    std::cout << "result of dt.subtract(): " << resultDT << std::endl;
+
+    resultDT = dt.multiply(3.2, 23. );        
+    std::cout << "result of dt.multiply(): " << resultDT << std::endl;
+
+    resultDT = dt.divide(3.2, 23. );     
+    std::cout << "result of dt.divide(): " << resultDT << std::endl;
+
+    IntType it;
+
+    auto resultIT = it.add(3, 23 );       
+    std::cout << "result of it.add(): " << resultIT << std::endl;
+
+    resultIT = it.subtract(3, 23 );
+    std::cout << "result of it.subtract(): " << resultIT << std::endl;
+
+    resultIT = it.multiply(3, 23 );        
+    std::cout << "result of it.multiply(): " << resultIT << std::endl;
+
+    resultIT = it.divide(3, 23 );     
+    std::cout << "result of it.divide(): " << resultIT << std::endl;
+
     std::cout << "good to go!" << std::endl;
 }
