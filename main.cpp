06b919b4a1eb643cacc5c751398943b11a9fbbad
Charles Schiermeyer <matkatmusic@gmail.com>
2020-05-06 19:28:05 -0700

    Added part1 instructions

--- main.cpp
+++ main.cpp
@@ -1,7 +1,61 @@
-#include <iostream>
+/*
+ Project 4 - Part 1 / 9
+ video: Chapter 2 Part 7
+ Returning Values tasks 
 
+ Create a branch named Part1
+
+ Purpose:  This project will take you through the process of writing a class that wraps a numeric type, beginning with writing simple member functions and ending with a fully templated class with lots of functionality. 
+ 
+ 1) write 3 UDTs named FloatType, DoubleType, IntType.
+ 
+ 2) give each UDT the following member functions:
+        add( lhs, rhs );
+        subtract( lhs, rhs );
+        multiply( lhs, rhs );
+        divide( lhs, rhs );
+
+       lhs is the left-hand-side of the operation, rhs is the right-hand-side
+       e.g. x = 2 + 3
+              '2' is the lhs
+              '3' is the rhs
+              '+' is the operation
+
+ 3) implement the appropriate action in the member function. 
+         a) Be aware that floating point division by 0 is legal, but integer division by 0 will crash your program.  
+         b) Handle this possible input when you write your divide() functions.
+         c) you should warn the user if they're doing floating-point-division-by-zero but not prevent the division from happening
+         d) you should warn AND prevent the division from happening if it is an integer-division-by-zero.
+ 
+ 4) make them return the correct primitive type. e.g. if you're implementing the FloatType::add function, your implementation would start like this:
+        float FloatType::add( float lhs, float rhs )
+ 
+ 5) declare a few instances of each class in main() and call the member functions with the appropriate arguments.
+        FloatType ft;
+        auto result = ft.add(3.2f, 23.f );
+ 
+ 6) print out those results using std::cout:
+        std::cout "result of ft.add(): " << result << std::endl;
+ 
+ 7) After you finish defining each type/function, click the [run] button.  Clear up any errors or warnings as best you can.
+ */
+
+/*
+ MAKE SURE YOU ARE NOT ON THE MASTER BRANCH
+
+ Commit your changes by clicking on the Source Control panel on the left, entering a message, and click [Commit and push].
+ 
+ If you didn't already: 
+    Make a pull request after you make your first commit
+    pin the pull request link and this repl.it link to our DM thread in a single message.
+
+ send me a DM to review your pull request when the project is ready for review.
+
+ Wait for my code review.
+ */
+
+#include <iostream>
 int main()
 {
     std::cout << "good to go!" << std::endl;
-    return 0;    
 }
