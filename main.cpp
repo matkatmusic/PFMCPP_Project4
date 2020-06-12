76e6c57fadac7dd45f8ab5051e6148462213983e
Eric Lennartson <lennartsoneric@gmail.com>
2020-06-12 13:39:18 +0000

    changed vt math funds to op overloads in part3

--- main.cpp
+++ main.cpp
@@ -403,13 +403,21 @@ void part3()
     DoubleType pi( 3.14 );
 
     std::cout << "The result of FloatType^4 divided by IntType is: " << ( ft *= (ft * ft) / it ) << std::endl;
-    // std::cout << "The result of DoubleType times 3 plus IntType is : " << dt.multiply( static_cast<double>(3) ).add( static_cast<int>(it) ) << std::endl;
-    // std::cout << "The result of IntType divided by 3.14 multiplied by DoubleType minus FloatType is: " << it.divide( static_cast<int>(pi) ).multiply( static_cast<int>(dt) ).subtract( static_cast<int>(ft) ) << std::endl;
-    // std::cout << "An operation followed by attempts to divide by 0, which are ignored and warns user: " << std::endl;
-    // std::cout << it.multiply( static_cast<int>(it) ).divide(0).divide( static_cast<int>(0.0f) ).divide( static_cast<int>(0.0) ) <<std::endl;
-    
-    // std::cout << "FloatType x IntType  =  " << it.multiply( static_cast<int>(ft) )<< std::endl;
-    // std::cout << "(IntType + DoubleType + FloatType) x 24 = " << it.add( static_cast<int>(dt) ).add( static_cast<int>(ft) ).multiply( 24 ) << std::endl;
+    std::cout << "The result of DoubleType times 3 plus IntType is : " << ( dt *= 3 + it ) << std::endl;
+    it /= static_cast<int>(3.14); // lose digits after decimal
+    it *= static_cast<int>(dt);
+    it -= static_cast<int>(ft);
+    std::cout << "The result of IntType divided by 3.14 multiplied by DoubleType minus FloatType is: " << it << std::endl;
+    std::cout << "An operation followed by attempts to divide by 0, which are ignored and warns user: " << std::endl;
+    it *= it;
+    it /= 0;
+    it /= static_cast<int>(0.f);
+    it /= static_cast<int>(0.0);
+    std::cout << it <<std::endl;
+    std::cout << "FloatType x IntType  =  " << ( it *= static_cast<int>(ft) )<< std::endl;
+    it += static_cast<int>(dt) + static_cast<int>(ft);
+    it *= 24;
+    std::cout << "(IntType + DoubleType + FloatType) x 24 = " << it << std::endl;
 }
 
 // void part4()
