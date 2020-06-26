73f72d7425505a84a26d48b6a689defed5214508
matkatmusic <matkatmusic@gmail.com>
2020-06-26 15:04:17 -0700

    added part 9

--- main.cpp
+++ main.cpp
@@ -1,68 +1,39 @@
+/*
+Project 4: Part 9 / 9
+ video: Chapter 5 Part 8
 
+Create a branch named Part9
+
+ Rule of 3-5-0 and S.R.P.
+ 
+ DO NOT EDIT YOUR PREVIOUS main(). 
+ 
+ 1) add the Leak Detector files from Project5
+ 
+ 2) add these macros after the JUCE_LEAK_DETECTOR macro :
+ */
+
+#define JUCE_DECLARE_NON_COPYABLE(className) \
+            className (const className&) = delete;\
+            className& operator= (const className&) = delete;
+
+#define JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(className) \
+            JUCE_DECLARE_NON_COPYABLE(className) \
+            JUCE_LEAK_DETECTOR(className)
 
 /*
-Prject 4: Part 8 / 9
- video: Chapter 5 Part 6 Task 
-Create a branch named Part8
- R-Values L-Values Move Semantics.
- This Task is going to demonstrate R-Values and writing Generic Code
-    Build/Run often with this task to make sure you're not breaking the code with each step.
-    I recommend committing after you get each step working so you can revert to a working version easily if needed.
-    it's very easy to mess this task up. 
+ 3) add JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(Temporary) to the end of the  Temporary<> struct
  
- Edit your Chapter 5 Part 4 Task and replace its instructions with these instructions
+ 4) add JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(Numeric) to the end of the Numeric<> struct
  
- Your job is to replace the owned type (the primitive specified by your template argument) from the 
-     Templated Class you created in Ch5 p04 with a struct named Temporary that can behave ENTIRELY as a temporary object.
+ if you compile it, you'll see lots of errors about deleted functions.
  
- That means you must use conversion functions to interact with what it owns.
+ 5) Implement the Rule of 5 on Numeric<> and Temporary<> to fix this.
  
- You need to figure out how to use conversion functions to be able to GET and SET the 'value' member variable.
-    hint: conversion functions can return by value and also by ___...
-  
- 1) Here is a starting point for how to implement your Temporary struct.
+ You should end up with the same program output as Part 8's task if you did it right.
  */
 
 /*
- 2) add the definition of Temporary::counter here, which is a static variable and must be defined outside of the class.
-    Remember the rules about how to define a Template member variable/function outside of the class.
-*/
-
-/*
- 3) You'll need to template your overloaded math operator functions in your Templated Class from Ch5 p04
-    use static_cast to convert whatever type is passed in to your template's NumericType before performing the +=, -=, etc.  here's an example implementation:
-namespace example
-{
-template<typename NumericType>
-struct Numeric
-{
-    //snip
-    template<typename OtherType>
-    Numeric& operator-=(const OtherType& o) 
-    { 
-        *value -= static_cast<NumericType>(o); 
-        return *this; 
-    }
-    //snip
-};
-}
-
- 4) remove your specialized <double> template of your Numeric<T> class from the previous task (ch5 p04)
-    replace the 2 apply() functions in your Numeric<T> with the single templated apply() function from the specialized <double> template.
-
- 5) Template your pow() function the same way you templated the overloaded math operators
-    Remove the call to powInternal() and just call std::pow() directly.
-    you'll need to static_cast<> the pow() argument the same way you did in the overloaded math operators, when you pass it to std::pow()
-
- 
- 6) Finally, your conversion function in your templated class is going to be returning this Temporary, 
-        so you should probably NOT return by copy if you want your templated class's owned object to be modified by any math operation.
-    See the previous hint for implementing the conversion functions for the Temporary if you want to get the held value
-*/
-
-
-/*
- 7)   replace main() with the main below
  If you did everything correctly, this is the output you should get:
  
 I'm a Temporary<f> object, #0
@@ -94,89 +65,10 @@ I'm a Temporary<i> object, #4
 i squared: 81
 I'm a Temporary<i> object, #5
 i cubed: 531441
+
 Use a service like https://www.diffchecker.com/diff to compare your output. 
-#include <iostream>
-int main()
-{
-    Numeric<float> f(0.1f);
-    Numeric<int> i(3);
-    Numeric<double> d(4.2);
-    
-    f += 2.f;
-    f -= i;
-    f *= d;
-    f /= 2.f;
-    std::cout << "f: " << f << std::endl;
-    
-    d += 2.f;
-    d -= i;
-    d *= f;
-    d /= 2.f;
-    std::cout << "d: " << d << std::endl;
-    
-    i += 2.f; i -= f; i *= d; i /= 2.f;
-    std::cout << "i: "<< i << std::endl;
-    
-    Point p(f, i);
-    p.toString();
-    
-    d *= -1;
-    std::cout << "d: " << d << std::endl;
-    
-    p.multiply(d.pow(f).pow(i));
-    std::cout << "d: " << d << std::endl;
-    
-    p.toString();
-    
-    Numeric<float> floatNum(4.3f);
-    Numeric<int> intNum(2);
-    Numeric<int> intNum2(6);
-    intNum = 2 + (intNum2 - 4) + static_cast<double>(floatNum) / 2.3;
-    std::cout << "intNum: " << intNum << std::endl;
-    
-    {
-        using Type = decltype(f)::Type;
-        f.apply([&f](std::unique_ptr<Type>&value) -> decltype(f)&
-                {
-                    auto& v = *value;
-                    v = v * v;
-                    return f;
-                });
-        std::cout << "f squared: " << f << std::endl;
-        
-        f.apply( cube<Type> );
-        std::cout << "f cubed: " << f << std::endl;
-    }
-    
-    {
-        using Type = decltype(d)::Type;
-        d.apply([&d](std::unique_ptr<Type>&value) -> decltype(d)&
-                {
-                    auto& v = *value;
-                    v = v * v;
-                    return d;
-                });
-        std::cout << "d squared: " << d << std::endl;
-        
-        d.apply( cube<Type> );
-        std::cout << "d cubed: " << d << std::endl;
-    }
-    
-    {
-        using Type = decltype(i)::Type;
-        i.apply([&i](std::unique_ptr<Type>&value) -> decltype(i)&
-                {
-                    auto& v = *value;
-                    v = v * v;
-                    return i;
-                });
-        std::cout << "i squared: " << i << std::endl;
-        
-        i.apply( cube<Type> );
-        std::cout << "i cubed: " << i << std::endl;
-    }
-}
 */
+
 
 #include <iostream>
 #include <cmath>
