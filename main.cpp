6c85827b8e6ff1dd2f32cac69d5105031dd98e98
DiaAudi <61022208+DiaAudi@users.noreply.github.com>
2021-09-13 02:34:59 +0000

    x

--- main.cpp
+++ main.cpp
@@ -577,74 +577,61 @@ void part4()
 }
 
 
-int main() 
+int main()
 {
-	// testing instruction 0
-	HeapA heapA;
+    //testing instruction 0
+    HeapA heapA; 
 
-	// assign heap primitives
-	FloatType ft(2.0f);
-	DoubleType dt(2);
-	IntType it(2);
+    //assign heap primitives
+    FloatType ft ( 2.0f );
+    DoubleType dt ( 2 );
+    IntType it ( 2 ) ;
 
-	std::cout << "FloatType add result=" << ft.add(2.0f) << std::endl;
-	std::cout << "FloatType subtract result=" << ft.subtract(2.0f) << std::endl;
-	std::cout << "FloatType multiply result=" << ft.multiply(2.0f) << std::endl;
-	std::cout << "FloatType divide result=" << ft.divide(16.0f) << std::endl
-			  << std::endl;
+    std::cout << "FloatType add result=" << ft.add( 2.0f ) << std::endl;
+    std::cout << "FloatType subtract result=" << ft.subtract( 2.0f ) << std::endl;
+    std::cout << "FloatType multiply result=" << ft.multiply( 2.0f ) << std::endl;
+    std::cout << "FloatType divide result=" << ft.divide( 16.0f) << std::endl << std::endl;
 
-	std::cout << "DoubleType add result=" << dt.add(2.0) << std::endl;
-	std::cout << "DoubleType subtract result=" << dt.subtract(2.0) << std::endl;
-	std::cout << "DoubleType multiply result=" << dt.multiply(2.0) << std::endl;
-	std::cout << "DoubleType divide result="
-			  << dt.divide(static_cast<double>(5.f)) << std::endl
-			  << std::endl;
+    std::cout << "DoubleType add result=" << dt.add(2.0) << std::endl;
+    std::cout << "DoubleType subtract result=" << dt.subtract(2.0) << std::endl;
+    std::cout << "DoubleType multiply result=" << dt.multiply(2.0) << std::endl;
+    std::cout << "DoubleType divide result=" << dt.divide(static_cast<double>(5.f)) << std::endl << std::endl;
 
-	std::cout << "IntType add result=" << it.add(2) << std::endl;
-	std::cout << "IntType subtract result=" << it.subtract(2) << std::endl;
-	std::cout << "IntType multiply result=" << it.multiply(2) << std::endl;
-	std::cout << "IntType divide result=" << it.divide(3) << std::endl
-			  << std::endl;
-	std::cout << "Chain calculation = "
-			  << (it.multiply(1000).divide(2).subtract(10).add(100))
-			  << std::endl;
+    std::cout << "IntType add result=" << it.add(2) << std::endl;
+    std::cout << "IntType subtract result=" << it.subtract(2) << std::endl;
+    std::cout << "IntType multiply result=" << it.multiply(2) << std::endl;
+    std::cout << "IntType divide result=" << it.divide(3) << std::endl << std::endl;
+    std::cout << "Chain calculation = " << (it.multiply(1000).divide(2).subtract(10).add(100)) << std::endl;
 
-	// FloatType object instanciation and method tests
-	// --------
-	std::cout << "New value of ft = (ft + 3.0f) * 1.5f / 5.0f = "
-			  << ft.add(3.0f).multiply(1.5f).divide(5.0f) << std::endl;
+        // FloatType object instanciation and method tests
+    // --------
+    std::cout << "New value of ft = (ft + 3.0f) * 1.5f / 5.0f = " << ft.add( 3.0f ).multiply(1.5f).divide(5.0f) << std::endl;
+       
+    std::cout << "---------------------\n" << std::endl; 
+    
+    // DoubleType/IntType object instanciation and method tests
+    // --------
+    std::cout << "Initial value of dt: " << dt << std::endl;
+    std::cout << "Initial value of it: " << it << std::endl;
+    // --------
+    std::cout << "Use of function concatenation (mixed type arguments) " << std::endl;
+    std::cout << "New value of dt = (dt * it) / 5.0f + ft = " << dt.multiply(it).divide(static_cast<double>(5.0f)).add(static_cast<double>(ft)) << std::endl;
 
-	std::cout << "---------------------\n" << std::endl;
+    std::cout << "---------------------\n" << std::endl; 
+    
+    // Intercept division by 0
+    // --------
+    std::cout << "Intercept division by 0 " << std::endl;
+    std::cout << "New value of it = it / 0 = " << it.divide(0) << std::endl;
+    std::cout << "New value of ft = ft / 0 = " << ft.divide(0) << std::endl;
+    std::cout << "New value of dt = dt / 0 = " << dt.divide(0) << std::endl;
 
-	// DoubleType/IntType object instanciation and method tests
-	// --------
-	std::cout << "Initial value of dt: " << dt << std::endl;
-	std::cout << "Initial value of it: " << it << std::endl;
-	// --------
-	std::cout << "Use of function concatenation (mixed type arguments) "
-			  << std::endl;
-	std::cout << "New value of dt = (dt * it) / 5.0f + ft = "
-			  << dt.multiply(it)
-					 .divide(static_cast<double>(5.0f))
-					 .add(static_cast<double>(ft))
-			  << std::endl;
+    std::cout << "---------------------\n" << std::endl; 
 
-	std::cout << "---------------------\n" << std::endl;
+    part3();
+    part4();
+    
+    std::cout << "good to go!\n";
 
-	// Intercept division by 0
-	// --------
-	std::cout << "Intercept division by 0 " << std::endl;
-	std::cout << "New value of it = it / 0 = " << it.divide(0) << std::endl;
-	std::cout << "New value of ft = ft / 0 = " << ft.divide(0) << std::endl;
-	std::cout << "New value of dt = dt / 0 = " << dt.divide(0) << std::endl;
-
-	std::cout << "---------------------\n" << std::endl;
-
-	part3();
-
-	part4();
-
-	std::cout << "good to go!\n";
-
-	return 0;
+    return 0;
 }
