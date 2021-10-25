aa404c231cd21ed1e8816124b0b9d8db7b99fbe5
josh-flori <joshman108@sbcglobal.net>
2021-10-25 23:26:16 +0000

    Added UDTs and filled out methods

--- main.cpp
+++ main.cpp
@@ -91,7 +91,73 @@ good to go!
  Wait for my code review.
  */
 
+
 #include <iostream>
+struct FloatType 
+{ 
+	float add( float lhs, float rhs );
+    float subtract( float lhs, float rhs );
+    float multiply( float lhs, float rhs );
+    float divide( float lhs, float rhs );
+};
+
+float FloatType::add (float lhs, float rhs) {return lhs + rhs;}
+float FloatType::subtract ( float lhs, float rhs ) {return lhs - rhs;}
+float FloatType::multiply ( float lhs, float rhs ) {return lhs * rhs;}
+float FloatType::divide ( float lhs, float rhs ) 
+{
+	if (rhs == 0.0f)
+	{
+		std::cout << "warning, floating point division by zero returns 'inf' !\n";
+	}
+	return lhs / rhs;
+}
+
+
+struct DoubleType 
+{ 
+	double add( double lhs, double rhs );
+    double subtract( double lhs, double rhs );
+    double multiply( double lhs, double rhs );
+    double divide( double lhs, double rhs );
+}; 
+
+double DoubleType::add ( double lhs, double rhs ) {return lhs + rhs;}
+double DoubleType::subtract ( double lhs, double rhs ) {return lhs - rhs;}
+double DoubleType::multiply ( double lhs, double  rhs ) {return lhs * rhs;}
+double DoubleType::divide ( double lhs, double rhs ) 
+{
+	if (rhs == 0.0)
+	{
+		std::cout << "warning, floating point division by zero returns 'inf' !\n";
+	}
+	return lhs / rhs;
+}
+
+struct IntType 
+{ 
+	int add( int lhs, int rhs );
+    int subtract( int lhs, int rhs );
+    int multiply( int lhs, int rhs );
+    int divide( int lhs, int rhs );
+};
+
+int IntType::add ( int lhs, int rhs) {return lhs + rhs;}
+int IntType::subtract ( int lhs, int rhs) {return lhs - rhs;}
+int IntType::multiply ( int lhs, int rhs) {return lhs * rhs;}
+int IntType::divide ( int lhs, int rhs) 
+{
+	if (rhs == 0)
+	{
+		std::cout << "error, integer division by zero will crash the program!\nreturning lhs\n";
+        return lhs;
+	}
+	else
+	{
+		return lhs / rhs;
+	}
+}
+
 int main() 
 {
     FloatType ft;
@@ -129,5 +195,3 @@ int main()
 
     std::cout << "good to go!" << std::endl;
 }
-
-
