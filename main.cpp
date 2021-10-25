4882de3547677c905ca6d01db21b8b57ff7d6a94
josh-flori <joshman108@sbcglobal.net>
2021-10-25 23:56:09 +0000

    Indentation fixes

--- main.cpp
+++ main.cpp
@@ -95,7 +95,7 @@ good to go!
 #include <iostream>
 struct FloatType 
 { 
-	float add( float lhs, float rhs );
+    float add( float lhs, float rhs );
     float subtract( float lhs, float rhs );
     float multiply( float lhs, float rhs );
     float divide( float lhs, float rhs );
@@ -106,17 +106,17 @@ float FloatType::subtract ( float lhs, float rhs ) {return lhs - rhs;}
 float FloatType::multiply ( float lhs, float rhs ) {return lhs * rhs;}
 float FloatType::divide ( float lhs, float rhs ) 
 {
-	if (rhs == 0.0f)
-	{
-		std::cout << "warning, floating point division by zero returns 'inf' !\n";
-	}
-	return lhs / rhs;
+    if (rhs == 0.0f)
+    {
+        std::cout << "warning, floating point division by zero returns 'inf' !\n";
+    }
+    return lhs / rhs;
 }
 
 
 struct DoubleType 
 { 
-	double add( double lhs, double rhs );
+    double add( double lhs, double rhs );
     double subtract( double lhs, double rhs );
     double multiply( double lhs, double rhs );
     double divide( double lhs, double rhs );
@@ -127,16 +127,16 @@ double DoubleType::subtract ( double lhs, double rhs ) {return lhs - rhs;}
 double DoubleType::multiply ( double lhs, double  rhs ) {return lhs * rhs;}
 double DoubleType::divide ( double lhs, double rhs ) 
 {
-	if (rhs == 0.0)
-	{
-		std::cout << "warning, floating point division by zero returns 'inf' !\n";
-	}
-	return lhs / rhs;
+    if (rhs == 0.0)
+    {
+        std::cout << "warning, floating point division by zero returns 'inf' !\n";
+    }
+    return lhs / rhs;
 }
 
 struct IntType 
 { 
-	int add( int lhs, int rhs );
+    int add( int lhs, int rhs );
     int subtract( int lhs, int rhs );
     int multiply( int lhs, int rhs );
     int divide( int lhs, int rhs );
@@ -147,15 +147,13 @@ int IntType::subtract ( int lhs, int rhs) {return lhs - rhs;}
 int IntType::multiply ( int lhs, int rhs) {return lhs * rhs;}
 int IntType::divide ( int lhs, int rhs) 
 {
-	if (rhs == 0)
-	{
-		std::cout << "error, integer division by zero will crash the program!\nreturning lhs\n";
+    if (rhs == 0)
+    {
+        std::cout << "error, integer division by zero will crash the program!\nreturning lhs\n";
         return lhs;
-	}
-	else
-	{
-		return lhs / rhs;
-	}
+    }
+    
+    return lhs / rhs;
 }
 
 int main() 
