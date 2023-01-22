2221de6e4fe9109c70f95d6198c2c11762541b0d
C. Dragon Music <119961784+CDragonMusic@users.noreply.github.com>
2023-01-22 03:01:21 +0000

    Fixed all the warnings.

--- main.cpp
+++ main.cpp
@@ -87,7 +87,8 @@ FloatType x IntType  =  13143546
 good to go!
 */
 
-#include <iostream>
+#include <iostream>  
+#include <cmath> 
 
 struct A {};
 struct HeapA
@@ -274,14 +275,14 @@ void part3()
     IntType it ( 34 );
     DoubleType pi( 3.14 );
 
-    std::cout << "The result of FloatType^4 divided by IntType is: " << ft.multiply( ft ).multiply( ft ).divide( it ) << std::endl;
-    std::cout << "The result of DoubleType times 3 plus IntType is : " << dt.multiply( 3 ).add( it ) << std::endl;
-    std::cout << "The result of IntType divided by 3.14 multiplied by DoubleType minus FloatType is: " << it.divide( pi ).multiply( dt ).subtract( ft ) << std::endl;
+    std::cout << "The result of FloatType^4 divided by IntType is: " << ft.multiply( ft ).multiply( ft ).divide(static_cast<float>( it )) << std::endl;
+    std::cout << "The result of DoubleType times 3 plus IntType is : " << dt.multiply( 3 ).add(static_cast<double>( it )) << std::endl;
+    std::cout << "The result of IntType divided by 3.14 multiplied by DoubleType minus FloatType is: " << it.divide(static_cast<int>(std::round(pi))).multiply(static_cast<int>(std::round(dt))).subtract(static_cast<int>(std::round(ft))) << std::endl;
     std::cout << "An operation followed by attempts to divide by 0, which are ignored and warns user: " << std::endl;
-    std::cout << it.multiply(it).divide(0).divide(0.0f).divide(0.0) << std::endl;
+    std::cout << it.multiply(it).divide(0).divide(static_cast<int>(std::round(0.0f))).divide(static_cast<int>(0.0)) << std::endl;
     
-    std::cout << "FloatType x IntType  =  " << it.multiply( ft ) << std::endl;
-    std::cout << "(IntType + DoubleType + FloatType) x 24 = " << it.add( dt ).add( ft ).multiply( 24 ) << std::endl;
+    std::cout << "FloatType x IntType  =  " << it.multiply(static_cast<int>(std::round(ft))) << std::endl;
+    std::cout << "(IntType + DoubleType + FloatType) x 24 = " << it.add(static_cast<int>(std::round(dt))).add(static_cast<int>(std::round(ft))).multiply( 24 ) << std::endl;
 }
 
 // ========== MAIN ========== //
