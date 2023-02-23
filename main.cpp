6885327317b6b9af9da297f13eb0d32a4b72b6d9
Charles Spencer <83370974+loveslap@users.noreply.github.com>
2023-02-23 20:08:42 +0000

    finished part 1

--- main.cpp
+++ main.cpp
@@ -76,7 +76,89 @@ returning lhs
 10
 good to go!
 */
+#include <iostream>
+struct FloatType
+{
+    float add( float lhs, float rhs );
+    float subtract( float lhs, float rhs );
+    float multiply( float lhs, float rhs );
+    float divide( float lhs, float rhs );
+};
 
+struct DoubleType
+{
+    double add( double lhs, double rhs );
+    double subtract( double lhs, double rhs );
+    double multiply( double lhs, double rhs );
+    double divide( double lhs, double rhs );
+};
+
+struct IntType
+{
+    int add( int lhs, int rhs );
+    int subtract( int lhs, int rhs );
+    int multiply( int lhs, int rhs );
+    int divide( int lhs, int rhs );
+};
+
+float FloatType::add( float lhs, float rhs )
+{
+    return lhs + rhs; 
+}
+float FloatType::subtract( float lhs, float rhs )
+{
+    return lhs - rhs;
+}
+float FloatType::multiply( float lhs, float rhs )
+{
+    return lhs * rhs;
+}
+float FloatType::divide( float lhs, float rhs )
+{
+    if( rhs == 0.0f) std::cout <<   "\nwarning, floating point division by zero returns 'inf'";
+    return lhs/rhs;   
+}
+
+double DoubleType::add( double lhs, double rhs )
+{
+    return lhs + rhs; 
+}
+double DoubleType::subtract( double lhs, double rhs )
+{
+    return lhs - rhs;
+}
+double DoubleType::multiply( double lhs, double rhs )
+{
+    return lhs * rhs;
+}
+double DoubleType::divide( double lhs, double rhs )
+{
+    if( rhs == 0.0) std::cout <<   "\nwarning, floating point division by zero returns 'inf'";
+    return lhs/rhs;   
+}
+
+int IntType::add( int lhs, int rhs )
+{
+    return lhs + rhs; 
+}
+int IntType::subtract( int lhs, int rhs )
+{
+    return lhs - rhs;
+}
+int IntType::multiply( int lhs, int rhs )
+{
+    return lhs * rhs;
+}
+int IntType::divide( int lhs, int rhs )
+{
+    if( rhs == 0)
+    {
+        std::cout <<  "error, integer division by zero will crash the program!\n";
+        std::cout << "returning lhs\n";
+        return lhs;
+    }
+    else return lhs/rhs;   
+}
 /*
  MAKE SURE YOU ARE NOT ON THE MASTER BRANCH
 
