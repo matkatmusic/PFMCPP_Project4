3338ab87d53a2eb652de564a082f87a92ccd86fe
odagonzpfm1 <154253425+odagonzpfm1@users.noreply.github.com>
2023-12-20 19:37:59 +0000

    initial commit

--- main.cpp
+++ main.cpp
@@ -91,7 +91,54 @@ good to go!
  Wait for my code review.
  */
 
+
 #include <iostream>
+
+struct FloatType
+{
+    float add(float lhs, float rhs) { return lhs + rhs; }
+    float subtract(float lhs, float rhs) { return lhs - rhs; }
+    float multiply(float lhs, float rhs) { return lhs * rhs; }
+    float divide(float lhs, float rhs) 
+    { 
+        if(rhs == 0.0f) 
+        {
+            std::cout << "warning, floating point division by zero returns 'inf' !" << std::endl; 
+        }
+        return lhs / rhs; 
+    }
+};
+
+struct DoubleType
+{
+    double add(double lhs, double rhs) { return lhs + rhs; }
+    double subtract(double lhs, double rhs) { return lhs - rhs; }
+    double multiply(double lhs, double rhs) { return lhs * rhs; }
+    double divide(double lhs, double rhs)
+    {
+        if(rhs == 0.0)
+        {
+            std::cout << "warning, floating point division by zero returns 'inf' !" << std::endl;
+        }
+        return lhs / rhs;
+    }
+};
+
+struct IntType
+{
+    int add(int lhs, int rhs) { return lhs + rhs; }
+    int subtract(int lhs, int rhs) { return lhs - rhs; }
+    int multiply(int lhs, int rhs) { return lhs * rhs; }
+    int divide(int lhs, int rhs)
+    {
+        if(rhs == 0)
+        {
+            std::cout << "error, integer division by zero will crash the program!" << std::endl;
+        }
+        return lhs / rhs;
+    }
+};
+
 int main() 
 {
     FloatType ft;
